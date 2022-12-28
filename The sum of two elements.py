5bd70b94e05871ee6959f9c695cd8f7080f8806d
Devang Mehta <54257752+devang8@users.noreply.github.com>
2022-12-28 14:58:27 +0530

    Update The sum of two elements.py

--- The sum of two elements.py
+++ The sum of two elements.py
@@ -3,6 +3,6 @@ target = int(input())
 result = []
 for i in range(nums + 1):
   if(nums[i] + nums[i + 1]) == target:   #checking whether the indices of the list adds upto the target
-    result.append(i)
+    result.append(i)                     #adding indices to the result list for output purpose
     result.append(i + 1)
 print(result)
